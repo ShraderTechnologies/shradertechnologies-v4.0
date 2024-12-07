4684668b3ccdfa36f737fe4479b607a303a5c0b0
Jury Gregorio Jr <jury.devs@gmail.com>
2024-12-07 15:28:30 +0800

    fixed navbar

--- app/(frontend)/components/header/Header.tsx
+++ app/(frontend)/components/header/Header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
   };
 
   return (
-    <header className="w-full h-screen dark:bg-matteblack ">
+    <header className="w-full dark:bg-matteblack ">
       {/* Background Image Container */}
       <div
         className="absolute inset-0 bg-[url('/images/background-white.png')] bg-cover bg-center bg-no-repeat -z-10"
@@ -55,38 +55,37 @@ export default function Header() {
       ></div>
 
       {/* Header content */}
-      <div className="h-16">
-        <div className="container mx-auto h-full ">
-          {/* Top section of the header with social icons and a theme toggle */}
-          <div className="flex items-center justify-between h-full">
-            {/* Social media icons */}
-            <div className="flex items-center space-x-4 z-0">
-              {socialIcons.map(({ icon: Icon, href, label }) => (
-                <a
-                  key={label}
-                  href={href}
-                  aria-label={label}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {/* Applies dynamic CSS classes to the social media icon */}
-                  <Icon className={getIconClass(label)} />
-                </a>
-              ))}
-            </div>
+
+      <div className="container mx-auto h-full">
+        {/* Top section of the header with social icons and a theme toggle */}
+        <div className="flex items-center justify-between h-full">
+          {/* Social media icons */}
+          <div className="flex items-center space-x-4 z-0">
+            {socialIcons.map(({ icon: Icon, href, label }) => (
+              <a
+                key={label}
+                href={href}
+                aria-label={label}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {/* Applies dynamic CSS classes to the social media icon */}
+                <Icon className={getIconClass(label)} />
+              </a>
+            ))}
           </div>
-          {/* Top content section */}
-          <TopContent />
-          {/* Video button with popup */}
-          <VideoButton />
-          {/* Grid layout for additional header content */}
-          <div className="grid grid-cols-1 max-[855px]:grid-cols-1 min-[855px]:grid-cols-2 gap-4 custom-header ">
-            <div className="w-full min-h-[200px] ">
-              <LeftContent />
-            </div>
-            <div className="w-full min-h-[200px] ">
-              <ContentRight />
-            </div>
+        </div>
+        {/* Top content section */}
+        <TopContent />
+        {/* Video button with popup */}
+        <VideoButton />
+        {/* Grid layout for additional header content */}
+        <div className="grid grid-cols-1 max-[855px]:grid-cols-1 min-[855px]:grid-cols-2 gap-4 custom-header ">
+          <div className="w-full min-h-[200px] ">
+            <LeftContent />
+          </div>
+          <div className="w-full min-h-[200px] ">
+            <ContentRight />
           </div>
         </div>
       </div>
