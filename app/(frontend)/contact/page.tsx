301d4d9fecb7b12a4c3c4e34f53e519f40229191
Jury Gregorio Jr <jury.devs@gmail.com>
2024-12-10 11:18:02 +0800

    added dark mode contact page

--- app/(frontend)/contact/page.tsx
+++ app/(frontend)/contact/page.tsx
@@ -12,12 +12,14 @@ export const metadata: Metadata = {
 export default function page() {
   return (
     <>
-      <MainTop />
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-16 lg:py-20">
-        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-900 dark:text-white mb-8 text-center transition-colors duration-300">
-          Contact Us
-        </h1>
-        <Form />
+      <div className="w-full bg-[#EDEEEF] dark:bg-matteblack">
+        <MainTop />
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-16 lg:py-20">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-900 dark:text-white mb-8 text-center transition-colors duration-300">
+            Contact Us
+          </h1>
+          <Form />
+        </div>
       </div>
     </>
   );
